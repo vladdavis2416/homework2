c4d98b1345f0c1fd83f655492bb4edad7826b3cf
vladdavis2416 <sockolow.vladisl@gmail.com>
2024-07-14 17:34:48 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+Homeworks = 12
+hours = 1.5
+Name = "Python"
+Result = Homeworks/hours
+print("Курс:", Name, ",", "всего задач:", Homeworks,"," , "затрачено часов:", hours,",", "среднее время выполнения", hours/Homeworks )
